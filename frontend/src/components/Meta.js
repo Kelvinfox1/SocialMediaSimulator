17079f11df1d99ce44092e4830a78c2bd52438c5
kelvinfox1 <ngure.kelvin@live.com>
2024-08-03 11:43:35 +0300

    redeploy myfirmsmm

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,12 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To My firm smm',
-  description: 'We sell the best online services for cheap',
-  keywords: 'youtube likes, youtube views, youtube subscribers, facebook, instagram followers',
+  title:
+    '#1 SOCIAL MEDIA MARKETING PLATFORM IN KENYA FOR 5 YEARS! | myfirmsmm.com',
+  description:
+    'Myfirmsmm has the Cheapest SMM Panel and 100% High Quality for all social networks. Get the best Instagram panel today! Boost your Online presence',
+  keywords:
+    'buy youtube views in Kenya, boost Instagram profile in kenya, top social media marketing in kenya, buy facebook likes in Kenya, youtube Kenya offers, boost your online presence in kenya, Youtube Kenya, smm reseller panel Kenya, sosyalmedya panel, Tiktok panel Kenya, views and likes in Kenya, Kenya panel, online marketing in Kenya, pata likes, pata views kenya',
 }
 
 export default Meta
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import store from './store'
-import './bootstrap.min.css'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import axios from 'axios'
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://myfirmsmm.herokuapp.com/'
+axios.defaults.baseURL = "http://localhost:5000/";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
